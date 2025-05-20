e9bdba38f33be8fd64f3d03a77d8275fbba02b95
Ymid <yortegap7920@gmail.com>
2025-05-20 00:01:54 -0500

    Initial commit

---
+++ BackEnd-Clinica/src/main/resources/ClinicaDeYmidApplication.java
@@ -0,0 +1,13 @@
+package com.ClinicaDeYmid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaDeYmidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaDeYmidApplication.class, args);
+	}
+
+}
---
+++ BackEnd-Clinica/src/test/java/com/ClinicaDeYmid/ClinicaDeYmidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ClinicaDeYmid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaDeYmidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
